627d9ccb4d0f85bd4dba2f551b210339c754ac86
David Cloutman <david.cloutman@lampfire.com>
2026-03-29 06:28:07 -0500

    Added bootstrapping to CLI script.

--- cli.php
+++ cli.php
@@ -3,8 +3,22 @@
  * This is the entry point for the command line interface (`cli`) tool. Use
  * the `/cli` directory to store code specific to the `cli` tool.
  */
+
+declare(strict_types=1);
+
 namespace Cli;
 
-// We need to autoload both the `cli` dependencies.
+// We need to autoload both the `cli` and `app` dependencies.
 require_once __DIR__ . '/cli/vendor/autoload.php';
+require_once __DIR__ . '/app/vendor/autoload.php';
+
+use Throwable;
+
+try {
+    $application = ApplicationFactory::create();
+    exit($application->run($argv));
+} catch (Throwable $throwable) {
+    fwrite(STDERR, sprintf("CLI bootstrap error: %s\n", $throwable->getMessage()));
+    exit(1);
+}
 
